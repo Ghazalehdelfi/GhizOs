35c4ce9183687e4808bdea2d9163356970685581
Ghazalehdelfi <ghazalehdelfi@gmail.com>
2025-05-18 23:04:22 -0400

    fix input type

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,11 +4,13 @@ import TextareaAutosize from 'react-autosize-textarea'
 const styles =
   'w-full rounded-md text-primary px-4 py-2 text-primary bg-gray-1000 dark:bg-white dark:bg-opacity-5 bg-opacity-5 hover border-gray-200 dark:border-gray-700'
 
-export function Input(props) {
+export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
   return <input className={styles} {...props} />
 }
 
-export function Textarea({ maxRows = 8, rows = 1, ...props }) {
+type TextareaProps = React.ComponentProps<typeof TextareaAutosize>
+
+export function Textarea({ maxRows = 8, rows = 1, ...props }: TextareaProps) {
   return (
     <TextareaAutosize
       maxRows={maxRows}
@@ -19,6 +21,6 @@ export function Textarea({ maxRows = 8, rows = 1, ...props }) {
   )
 }
 
-export function Select(props) {
+export function Select(props: React.SelectHTMLAttributes<HTMLSelectElement>) {
   return <select className={styles} {...props} />
 }
