ce4ddce61598f0f27a7357445c4a7f258103071b
Ghazalehdelfi <ghazalehdelfi@gmail.com>
2025-07-26 00:25:53 -0400

    deactivate agent

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -10,42 +10,42 @@ export default async function handler(
 
   try {
     console.log('Sending request to A2A server:', req.body)
-    const response = await fetch(
-      'https://a2a-host-agent-695627813996.us-central1.run.app',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          method: 'tasks/send',
-          params: {
-            id: crypto.randomUUID(),
-            sessionId: req.body.sessionId,
-            message: {
-              role: 'user',
-              parts: [{ type: 'text', text: req.body.message }],
-            },
-            historyLength: null,
-            metadata: null,
-          },
-        }),
-      }
-    )
+    // const response = await fetch(
+    //   'https://a2a-host-agent-695627813996.us-central1.run.app',
+    //   {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //       method: 'tasks/send',
+    //       params: {
+    //         id: crypto.randomUUID(),
+    //         sessionId: req.body.sessionId,
+    //         message: {
+    //           role: 'user',
+    //           parts: [{ type: 'text', text: req.body.message }],
+    //         },
+    //         historyLength: null,
+    //         metadata: null,
+    //       },
+    //     }),
+    //   }
+    // )
 
-    if (!response.ok) {
-      throw new Error(`A2A server responded with status: ${response.status}`)
-    }
-    const data = await response.json()
-    console.log('Received response from A2A server:', data)
+    // if (!response.ok) {
+    //   throw new Error(`A2A server responded with status: ${response.status}`)
+    // }
+    // const data = await response.json()
+    // console.log('Received response from A2A server:', data)
 
-    if (!data || data.result.history.length === 0) {
-      throw new Error('Invalid response format from A2A server')
-    }
+    // if (!data || data.result.history.length === 0) {
+    //   throw new Error('Invalid response format from A2A server')
+    // }
 
     res.status(200).json({
       agent_response:
-        data.result.history[data.result.history.length - 1].parts[0].text,
+        'The agents are currently inactive! If you are interested in how the system works, please refer to my blog post for a video demo! https://delfig.dev/writing/SecretaryAgent',
     })
   } catch (error) {
     console.error('Error in chat API:', error)
